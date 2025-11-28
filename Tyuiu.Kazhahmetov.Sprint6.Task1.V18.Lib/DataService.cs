f8075dd1a3f8972945ee2587f997d513ae0298d1
Таджикович Ержан <kazamircik@gmail.com>
2025-11-29 02:43:35 +0500

    Task 1

--- Tyuiu.Kazhahmetov.Sprint6.Task1.V18.Lib/DataService.cs
+++ Tyuiu.Kazhahmetov.Sprint6.Task1.V18.Lib/DataService.cs
@@ -6,10 +6,10 @@ namespace Tyuiu.Kazhahmetov.Sprint6.Task1.V18.Lib
         public double[] GetMassFunction(int startValue, int stopValue)
         {
             double y;
-            int len = stopValue - startValue;
+            int len = stopValue - startValue + 1;
             double[] res = new double[len];
             int cnt = 0;
-            for (int i = startValue; i < stopValue; i++)
+            for (int i = startValue; i < stopValue + 1; i++)
             {
                 y = (2 * Math.Sin(i))/(3 * i + 1.2) + Math.Cos(i) - 7 * i * 2;
                 y = Math.Round(y, 2);
--- Tyuiu.Kazhahmetov.Sprint6.Task1.V18.Test/DataServiceTest.cs
+++ Tyuiu.Kazhahmetov.Sprint6.Task1.V18.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.Kazhahmetov.Sprint6.Task1.V18.Test
             DataService ds = new DataService();
             int startValue = -5;
             int stopValue = 5;
-            int len = stopValue - startValue;
+            int len = stopValue - startValue + 1;
             double[] wait = new double[len];
             wait[0] = 70.14;
             wait[1] = 55.21;
@@ -22,6 +22,7 @@ namespace Tyuiu.Kazhahmetov.Sprint6.Task1.V18.Test
             wait[7] = -28.16;
             wait[8] = -42.96;
             wait[9] = -56.77;
+            wait[10] = -69.83;
             double[] res = ds.GetMassFunction(startValue, stopValue);
             CollectionAssert.AreEqual(wait, res);
         }
