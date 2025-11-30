7250d32ce7d0ee0c042b274cb8073cef61e80567
Таджикович Ержан <kazamircik@gmail.com>
2025-11-30 14:03:54 +0500

    Task 3

--- Tyuiu.Kazhahmetov.Sprint6.Task3.V23/FormMain.Designer.cs
+++ Tyuiu.Kazhahmetov.Sprint6.Task3.V23/FormMain.Designer.cs
@@ -28,19 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
+            groupBoxTask = new GroupBox();
+            dataGridViewTask = new DataGridView();
+            textBox1 = new TextBox();
+            buttonRes = new Button();
+            buttonInfo = new Button();
+            groupBoxRes = new GroupBox();
+            dataGridViewRes = new DataGridView();
+            textBoxRes = new TextBox();
+            groupBoxTask.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTask).BeginInit();
+            groupBoxRes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewRes).BeginInit();
             SuspendLayout();
+            // 
+            // groupBoxTask
+            // 
+            groupBoxTask.Controls.Add(dataGridViewTask);
+            groupBoxTask.Controls.Add(textBox1);
+            groupBoxTask.Location = new Point(12, 12);
+            groupBoxTask.Name = "groupBoxTask";
+            groupBoxTask.Size = new Size(382, 294);
+            groupBoxTask.TabIndex = 0;
+            groupBoxTask.TabStop = false;
+            groupBoxTask.Text = "Условие";
+            groupBoxTask.Enter += groupBox1_Enter;
+            // 
+            // dataGridViewTask
+            // 
+            dataGridViewTask.AllowUserToAddRows = false;
+            dataGridViewTask.AllowUserToDeleteRows = false;
+            dataGridViewTask.AllowUserToResizeColumns = false;
+            dataGridViewTask.AllowUserToResizeRows = false;
+            dataGridViewTask.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewTask.ColumnHeadersVisible = false;
+            dataGridViewTask.Location = new Point(18, 60);
+            dataGridViewTask.Name = "dataGridViewTask";
+            dataGridViewTask.ReadOnly = true;
+            dataGridViewTask.RowHeadersVisible = false;
+            dataGridViewTask.Size = new Size(345, 213);
+            dataGridViewTask.TabIndex = 1;
+            dataGridViewTask.CellContentClick += dataGridViewTask_CellContentClick;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(6, 19);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(370, 268);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Дан массив 5 на 5 элементов. Выполнить сортировку по возрастанию во втором столбце.";
+            // 
+            // buttonRes
+            // 
+            buttonRes.BackColor = Color.Lime;
+            buttonRes.Location = new Point(287, 319);
+            buttonRes.Name = "buttonRes";
+            buttonRes.Size = new Size(224, 56);
+            buttonRes.TabIndex = 1;
+            buttonRes.Text = "Выполнить";
+            buttonRes.UseVisualStyleBackColor = false;
+            buttonRes.Click += buttonRes_Click;
+            buttonRes.MouseClick += buttonRes_MouseClick;
+            buttonRes.MouseEnter += buttonRes_MouseEnter;
+            buttonRes.MouseLeave += buttonRes_MouseLeave;
+            // 
+            // buttonInfo
+            // 
+            buttonInfo.BackColor = Color.Aqua;
+            buttonInfo.Location = new Point(362, 385);
+            buttonInfo.Name = "buttonInfo";
+            buttonInfo.Size = new Size(77, 56);
+            buttonInfo.TabIndex = 2;
+            buttonInfo.Text = "?";
+            buttonInfo.UseVisualStyleBackColor = false;
+            buttonInfo.Click += buttonInfo_Click;
+            // 
+            // groupBoxRes
+            // 
+            groupBoxRes.Controls.Add(dataGridViewRes);
+            groupBoxRes.Controls.Add(textBoxRes);
+            groupBoxRes.Location = new Point(415, 12);
+            groupBoxRes.Name = "groupBoxRes";
+            groupBoxRes.Size = new Size(362, 294);
+            groupBoxRes.TabIndex = 3;
+            groupBoxRes.TabStop = false;
+            groupBoxRes.Text = "Результат";
+            groupBoxRes.Enter += groupBox1_Enter_1;
+            // 
+            // dataGridViewRes
+            // 
+            dataGridViewRes.AllowUserToAddRows = false;
+            dataGridViewRes.AllowUserToDeleteRows = false;
+            dataGridViewRes.AllowUserToResizeColumns = false;
+            dataGridViewRes.AllowUserToResizeRows = false;
+            dataGridViewRes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewRes.ColumnHeadersVisible = false;
+            dataGridViewRes.Location = new Point(16, 60);
+            dataGridViewRes.MultiSelect = false;
+            dataGridViewRes.Name = "dataGridViewRes";
+            dataGridViewRes.RowHeadersVisible = false;
+            dataGridViewRes.Size = new Size(331, 213);
+            dataGridViewRes.TabIndex = 0;
+            dataGridViewRes.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // textBoxRes
+            // 
+            textBoxRes.Location = new Point(6, 22);
+            textBoxRes.Multiline = true;
+            textBoxRes.Name = "textBoxRes";
+            textBoxRes.ReadOnly = true;
+            textBoxRes.Size = new Size(350, 265);
+            textBoxRes.TabIndex = 1;
+            textBoxRes.Text = "Готовая матрица:";
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(groupBoxRes);
+            Controls.Add(buttonInfo);
+            Controls.Add(buttonRes);
+            Controls.Add(groupBoxTask);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "FormMain";
+            RightToLeft = RightToLeft.No;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             Load += FormMain_Load;
+            groupBoxTask.ResumeLayout(false);
+            groupBoxTask.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewTask).EndInit();
+            groupBoxRes.ResumeLayout(false);
+            groupBoxRes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewRes).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBoxTask;
+        private TextBox textBox1;
+        private DataGridView dataGridViewTask;
+        private Button buttonRes;
+        private Button buttonInfo;
+        private GroupBox groupBoxRes;
+        private TextBox textBoxRes;
+        private DataGridView dataGridViewRes;
     }
 }
--- Tyuiu.Kazhahmetov.Sprint6.Task3.V23/FormMain.cs
+++ Tyuiu.Kazhahmetov.Sprint6.Task3.V23/FormMain.cs
@@ -1,3 +1,4 @@
+using Tyuiu.Kazhahmetov.Sprint6.Task3.V23.Lib;
 namespace Tyuiu.Kazhahmetov.Sprint6.Task3.V23
 {
     public partial class FormMain : Form
@@ -6,10 +7,127 @@ namespace Tyuiu.Kazhahmetov.Sprint6.Task3.V23
         {
             InitializeComponent();
         }
+        DataService ds = new DataService();
+        int[,] matrix = new int[5, 5] {
+                                {0, -19, 25, 34,  0 },
+                                {-19, -16, 1, -5, 32},
+                                {1, 13, -5, -17,  -5},
+                                {3, -9, -15, -1,   0},
+                                {1, 20, 15, -5,   31} };
+
+
 
         private void FormMain_Load(object sender, EventArgs e)
         {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
 
+            dataGridViewTask.ColumnCount = cols;
+            dataGridViewTask.RowCount = rows;
+
+
+            for (int i = 0; i < cols; i++)
+            {
+                dataGridViewTask.Columns[i].Width = 25;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    dataGridViewTask.Rows[i].Cells[j].Value = Convert.ToString(matrix[i, j]);
+                }
+
+            }
+            dataGridViewTask.ReadOnly = true;
+            dataGridViewTask.AllowUserToOrderColumns = false;
+            dataGridViewTask.AllowUserToResizeColumns = false;
+            dataGridViewTask.AllowUserToResizeRows = false;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 3 выполнил студент группы ПИНб-25-1 Кажахметов Амир Ерболович", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void dataGridViewTask_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
+
+            for (int i = 0; i < cols; i++)
+            {
+                dataGridViewTask.Columns[i].Width = 25;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    dataGridViewTask.Rows[i].Cells[j].Value = Convert.ToString(matrix[i, j]);
+                }
+
+            }
+        }
+
+        private void buttonRes_Click(object sender, EventArgs e)
+        {
+            int[,] res_matrix = ds.Calculate(matrix);
+
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
+
+            dataGridViewRes.ColumnCount = cols;
+            dataGridViewRes.RowCount = rows;
+
+
+            for (int i = 0; i < cols; i++)
+            {
+                dataGridViewRes.Columns[i].Width = 25;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    dataGridViewRes.Rows[i].Cells[j].Value = Convert.ToString(res_matrix[i, j]);
+                }
+
+            }
+            dataGridViewRes.ReadOnly = true;
+            dataGridViewRes.AllowUserToOrderColumns = false;
+            dataGridViewRes.AllowUserToResizeColumns = false;
+            dataGridViewRes.AllowUserToResizeRows = false;
+        }
+
+        private void buttonRes_MouseEnter(object sender, EventArgs e)
+        {
+            buttonRes.BackColor = Color.Red;
+        }
+
+        private void buttonRes_MouseLeave(object sender, EventArgs e)
+        {
+            buttonRes.BackColor = Color.Lime;
+        }
+
+        private void buttonRes_MouseClick(object sender, MouseEventArgs e)
+        {
+            buttonRes.BackColor = Color.Yellow;
         }
     }
 }
